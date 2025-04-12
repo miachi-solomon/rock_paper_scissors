acd9d052cb470ded79d0c55c5cd4f3e2a6c49d32
Miachi Solomon <smiachi10@gmail.com>
2025-04-12 23:07:35 +0100

    creating a function to get the computer's choice

---
+++ script.js
@@ -0,0 +1,14 @@
+let computerChoice = undefined;
+function getComputerChoice() {
+    // return rock paper or scissors using Math.random()
+    // use Math.random() to return a number from 1-3
+    let number = Math.round(Math.random() * 2);
+    // if Math.random() is 0 return Rock, if Math.random() is 1 return Paper, for any other value return Scissors
+    if (number == 0) {
+        return 'Rock';
+    } else if (number == 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
